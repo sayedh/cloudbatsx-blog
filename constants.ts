eac4a2bb553587784014f65f805a57cab42a96c6
cloudbats <cloudbatsx@gmail.com>
2026-01-12 13:59:36 -0800

    remove extra

--- constants.ts
+++
@@ -1,7 +0,0 @@
-// src/lib/constants.ts
-export const SITE_NAME = "cloudbatsX";
-export const SITE_URL = "https://blog.cloudbats.com";
-export const HOME_OG_IMAGE_URL =
-  "https://blog.cloudbats.com/assets/og-image.png";
-export const GITHUB_URL = "https://github.com/sayedh/cloudbatsx-blog";
-export const PORTFOLIO_URL = "https://sayedh.com";
